4ab6ff9fd0eeb9c884bc4f8148870c06d1b270ac
Tatyana1601 <118970445+Tatyana1601@users.noreply.github.com>
2023-06-29 15:04:40 +0300

    Add files via upload

---
+++ lesson_1/homework_1/script.js
@@ -0,0 +1,63 @@
+const value_1 = true;
+const result_1 = String(value_1);
+console.log(result_1);
+
+const value_2 = 1000;
+const result_2 = String(value_2);
+console.log(result_2);
+
+const value_3 = null;
+const result_3 = String(value_3);
+console.log(result_3);
+
+const value_4 = undefined;
+const result_4 = String(value_4);
+console.log(result_4);
+
+const num_1 = '123a';
+const result_num_1 = Number(num_1);
+console.log(result_num_1);
+
+const num_2 = '1000';
+const result_num_2 = Number(num_2);
+console.log(result_num_2);
+
+const num_3 = null;
+const result_num_3 = Number(num_3);
+console.log(result_num_3);
+
+const num_4 = undefined;
+const result_num_4 = Number(num_4);
+console.log(result_num_4);
+
+const num_5 = false;
+const result_num_5 = Number(num_5);
+console.log(result_num_5);
+
+const bool_1 = null;
+const result_bool_1 = Boolean(bool_1);
+console.log(result_bool_1);
+
+const bool_2 = undefined;
+const result_bool_2 = Boolean(bool_2);
+console.log(result_bool_2);
+
+const bool_3 = '';
+const result_bool_3 = Boolean(bool_3);
+console.log(result_bool_3);
+
+const bool_4 = 0;
+const result_bool_4 = Boolean(bool_4);
+console.log(result_bool_4);
+
+const bool_5 = ' ';
+const result_bool_5 = Boolean(bool_5);
+console.log(result_bool_5);
+
+const bool_6 = 1000;
+const result_bool_6 = Boolean(bool_6);
+console.log(result_bool_6);
+
+const bool_7 = '1200';
+const result_bool_7 = Boolean(bool_7);
+console.log(result_bool_7);
