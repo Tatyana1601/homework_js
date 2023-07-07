d96de61afd500e06b4bc882c660c6b8e350e9994
Tatyana1601 <118970445+Tatyana1601@users.noreply.github.com>
2023-07-07 14:08:50 +0300

    Add files via upload

--- lesson_5/homework_1/script.js
+++ lesson_5/homework_1/script.js
@@ -1,12 +1,8 @@
-const maxNumber = (a, b) => {
-    return a > b;
-}
+const maxNumber = (a, b) =>  a > b;
 const result_1 = maxNumber(15, 5);
 console.log(result_1);
 
-const checkAge = (age) => {
-    return age >= 18;
-}
+const checkAge = (age) => age >= 18;
 const result_2 = checkAge(19);
 console.log(result_2);
 
@@ -22,8 +18,6 @@ const cutString = (text, mLenght) =>{
 }
 cutString('Hello javascript', 5);
 
-const checkWord = (str, word) => {
-    return str.includes(word);
-}
+const checkWord = (str, word) => str.includes(word);
 const result_3 = checkWord('Hello javascript', 'Hello');
 console.log(result_3);
