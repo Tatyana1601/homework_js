d7acc8cd898046f66f3ed92abfbe89a50f166311
Tatyana1601 <118970445+Tatyana1601@users.noreply.github.com>
2023-08-17 21:20:03 +0300

    Add files via upload

---
+++ lesson_13/homework_1/script.js
@@ -0,0 +1,26 @@
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+let count = 0;
+const container = document.querySelector(".container");
+
+const handleContainerClick = (e) => {
+    const eventName = e.target.dataset.action;
+    if (eventName) {
+        switch (eventName) {
+             case "decrease":
+              count -= 1;
+              break;
+             case "reset":
+               count = 0;
+             break;
+             case "increase":
+               count += 1;
+                break;
+            default:
+              return;
+        }
+        
+   } value.innerHTML = count;
+}
+
+container.addEventListener("click", handleContainerClick)
