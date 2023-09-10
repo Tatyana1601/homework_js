1a24d4e323541be0c3b12d2affbdaadeaa1d74b9
Tatyana1601 <118970445+Tatyana1601@users.noreply.github.com>
2023-09-11 00:05:29 +0300

    Add files via upload

--- homeWork_19/script.js
+++ homeWork_19/script.js
@@ -1,8 +1,12 @@
  const getData = async (url) => {
-    
+        try{
         const response = await fetch("https://jsonplaceholder.typicode.com/users");
+       console.log(response.ok);
         const data = await response.json();
         console.log (data);  
+    }catch(e){
+        console.log("Error")
+    }
     }
 getData();
 
